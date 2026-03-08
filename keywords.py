e7a58fa49ca0c82cfb2734ff21590a7a1da46f80
promise-temi <temi.promisejohn@gmail.com>
2026-03-08 18:03:34 +0100

    .

---
+++ keywords.py
@@ -0,0 +1,236 @@
+
+KEYWORDS = [
+
+# --- IMMATRICULATION / CARTE GRISE ---
+"immat","immatriculation","immatriculer","immatriculé","immatriculee","immatricule",
+"carte grise","cartegrise","carte-grise","cartegris",
+"certificat d'immatriculation","certificat immatriculation",
+"plaque","plaques","plaque immat","plaque d'immatriculation",
+"vehicule","véhicule","voiture","auto","moto","scooter","remorque",
+"ants","ANTS","siv","SIV","cession","certificat de cession",
+"controle technique","contrôle technique",
+
+# --- ACTE DE NAISSANCE ---
+"naissance","naiss","né","nee","né(e)","naitre","naître",
+"acte","acte naissance","acte de naissance","acte-de-naissance",
+"extrait","extrait naissance","extrait de naissance",
+"copie","copie integrale","copie intégrale","copie d'acte",
+"certificat","certificat naissance",
+"etat civil","état civil","mairie","registre","filiation","sans filiation",
+
+# --- MARIAGE ---
+"mariage","mariag","marier","se marier","marié","marie","mariée","mariee","mariés","maries",
+"epoux","époux","epouse","épouse","conjoint","conjointe",
+"fiancé","fiance","fiancée","fiancee","fiancés","fiances","couple",
+"épouser","epouser","union","union civile",
+"dossier","dossier mariage","documents","papiers","piece","pieces","pièces",
+"bans","publication des bans","temoin","témoin","témoins","temoins",
+
+# --- DÉCÈS ---
+"décès","deces","dece","mort","decede","décédé","decedee","décédée",
+"declaration","déclaration","declarer","déclarer",
+"acte de décès","acte de deces","certificat de décès","certificat de deces",
+"obsèques","obseques","funerailles","funérailles",
+"inhumation","enterrement","cremation","crémation",
+"pompes funebres","pompes funèbres",
+
+# --- PACS ---
+"pacs","PACS","pacser","se pacser","pacsé","pacse","pacsée","pacsee",
+"pacte civil","pacte civil de solidarité","pacte",
+"convention pacs","dossier pacs","documents pacs",
+"partenaire","partenaires","concubinage","union libre","vie commune",
+"notaire","tribunal",
+
+# --- LIVRET DE FAMILLE ---
+"livret","livret famille","livret de famille","livret-de-famille",
+"duplicata","dupli","double","second livret",
+"perte","perdu","perdue","vol","vole","volé",
+"abime","abimé","abîmé","mise a jour","mise à jour","maj livret",
+
+# --- RECONNAISSANCE ENFANT ---
+"reconnaissance","reconnaitre","reconnaître","reconnu","reconnue",
+"enfant","bébé","bebe","nouveau né","nouveau-ne","nourrisson",
+"paternité","paternite","pere","père","mere","mère","filiation",
+"acte reconnaissance","acte de reconnaissance","autorité parentale",
+
+# --- CHANGEMENT PRÉNOM ---
+"prénom","prenom","prénoms","prenoms",
+"changer prénom","changer prenom","changement prénom","changement prenom",
+"modifier prénom","modif prenom","rectification","correction",
+"ajout prénom","suppression prénom","mention marginale",
+
+# --- PERMIS DE CONSTRUIRE ---
+"permis","permi","permis construire","permis de construire","pc","PC",
+"deposer","déposer","depot","dépôt","demande permis","dossier permis",
+"construction","construire","batir","bâtir","maison","batiment","bâtiment",
+"urbanisme","cerfa","plan","plans","surface","m2","m²",
+
+# --- EXTENSION ---
+"extension","extention","agrandissement","agrandir",
+"surelevation","surélévation","veranda","véranda","garage","annexe",
+"declaration prealable","déclaration préalable","dp","DP",
+
+# --- PISCINE ---
+"piscine","pisine","bassin","hors sol","hors-sol","enterrée","enterree",
+"abri piscine","local technique",
+
+# --- PLU ---
+"plu","PLU","plan local d'urbanisme","zonage","zone","parcelle",
+"cadastre","constructible","inconstructible","reglement","règlement",
+
+# --- TRAVAUX / DP ---
+"travaux","traveaux","modification","facade","façade","toiture",
+"cloture","clôture","portail",
+
+# --- ABRI DE JARDIN ---
+"abri","abri jardin","abri de jardin","abri-jardin",
+"cabanon","cabannon","chalet","remise",
+
+# --- ORDURES / COLLECTE ---
+"ordures","ordures menageres","ordures ménagères",
+"poubelle","poubelles","bac","ramassage","collecte",
+"jour collecte","calendrier collecte",
+
+# --- DÉCHETS VERTS ---
+"dechets verts","déchets verts","vegetaux","végétaux",
+"branchage","branchages","tonte","gazon","taille",
+"compost","compostage",
+
+# --- BAC ---
+"bac jaune","bac gris","bac vert","conteneur","container",
+"bac cassé","poubelle cassée","vol bac","perte bac",
+
+# --- TRI ---
+"tri","recyclage","emballages","plastique","carton","papier",
+"metal","métal","canette","bouteille",
+
+# --- PILES ---
+"pile","piles","batterie","batteries","accumulateur","accumulateurs",
+"recyclage piles","borne piles","dechets dangereux",
+
+# --- DÉCHETTERIE ---
+"dechetterie","déchetterie","decheterie","décheterie",
+"horaire","horaires","ouverture","fermeture",
+
+# --- ENCOMBRANTS ---
+"encombrant","encombrants","gros dechets","gros déchets",
+"meuble","canapé","canape","matelas","electromenager","électroménager",
+
+# --- TRANSPORT ---
+"transport scolaire","bus scolaire","carte transport","abonnement scolaire",
+"transport a la demande","transport à la demande","tad","TAD",
+"bus","horaires bus","ligne","arrêt","arret",
+
+# --- ENFANCE ---
+"creche","crèche","petite enfance","garde",
+"cantine","restauration scolaire","restaurant scolaire",
+"centre de loisirs","alsh","ALSH",
+"periscolaire","périscolaire","garderie",
+
+# --- SOCIAL ---
+"aide sociale","aides","ccas","CCAS","assistante sociale",
+"rsa","RSA","caf","CAF","allocation",
+
+# --- ÉNERGIE ---
+"energie","énergie","electricite","électricité","gaz",
+"facture","factures","cheque energie","chèque énergie","maprimerenov",
+
+# --- ASSOCIATION ---
+"association","asso","assoc",
+"creation association","création association","creer association","créer association",
+"statuts","bureau","président","president","trésorier","tresorier",
+"prefecture","préfecture","joafe","JOAFE"
+
+
+
+
+
+
+# --- GÉNÉRAL ADMINISTRATIF (TRÈS UTILISÉ PAR LES USAGERS) ---
+"papier","papiers","paperasse","document","documents","doc","docs",
+"dossier complet","mon dossier","le dossier",
+"formulaire","formulaire cerfa","cerfa","numero cerfa","numéro cerfa",
+"attestation","attest","justificatif","justif",
+"original","photocopie","copie papier","scan","pdf",
+"demande","faire une demande","faire le dossier","deposer un dossier",
+"envoyer","envoyer dossier","remplir","signature","signer",
+
+# --- ERREURS / FRAPPES COURANTES ---
+"permi","permis construire","permi construire",
+"decheterie","decheterie municipale","decheterie communale",
+"immatrikulation","immatrculation","imatriculation","imat",
+"declaration","declararion","decleration",
+"piscinne","pisicne","piscin",
+"cantinne","cantin",
+"dechet","dechets","deche","deché",
+"naissence","naissanse","naissancee",
+"mariag","marriage","marige",
+"livré","livret famille","livre de famille",
+
+# --- FORMULATIONS PARLÉES / ORALES ---
+"comment faire pour","comment je fais pour","je voudrais",
+"j'aimerais","j ai besoin","besoin de","où je dois aller",
+"où faut aller","c est ou","c'est où","c est quand",
+"ça se fait où","ça se passe où","qui contacter",
+"a qui m adresser","à qui m'adresser",
+"est ce que je peux","est-ce que je dois",
+"faut il","faut-il","ai-je le droit",
+
+# --- TEMPS / DÉLAIS / URGENCE ---
+"urgent","urgence","rapidement","au plus vite",
+"délai","delai","temps","combien de temps",
+"date limite","avant quand","quand","sous combien de jours",
+
+# --- LIEUX / CONTACT ---
+"mairie","hotel de ville","hôtel de ville","accueil mairie",
+"guichet","service","service municipal","service public",
+"bureau","bureau municipal","accueil",
+"telephone","téléphone","mail","email","contact",
+"rendez vous","rendez-vous","rdv",
+
+# --- PERSONNES / SITUATIONS ---
+"enfant","bébé","bebe","ado","adolescent",
+"parents","parent","famille","foyer",
+"personne agee","personne âgée","senior","sénior",
+"handicap","handicapé","handicapée","invalidite","invalidité",
+"proprietaire","propriétaire","locataire","voisin",
+
+# --- LOGEMENT / TRAVAUX (FLU) ---
+"ma maison","mon terrain","chez moi","mon logement",
+"travaux maison","faire des travaux","renover","rénover",
+"changer","modifier","agrandir","amenager","aménager",
+"cloture","clôture","mur","toit","toiture",
+"surface habitable","surface maison",
+
+# --- DÉCHETS / PROBLÈMES COURANTS ---
+"poubelle pleine","poubelle cassée","poubelle disparue",
+"ramassage pas fait","pas ramassé","oubli collecte",
+"odeur","sale","saleté","proprete","propreté",
+"depot sauvage","dépôt sauvage","depot illegal","dépôt illégal",
+
+# --- TRANSPORT / MOBILITÉ ---
+"aller au travail","aller a l ecole","aller à l'école",
+"se deplacer","se déplacer","trajet",
+"navette","transport communal","transport public",
+"pas de bus","ligne inexistante","horaires changes",
+
+# --- AIDES / ARGENT (FORMULATIONS USAGER) ---
+"aide financiere","aide financière","besoin d aide","besoin d'aide",
+"probleme argent","problème argent","difficulte financiere",
+"payer facture","payer moins","aide paiement",
+"je n arrive pas a payer","je n'arrive pas à payer",
+
+# --- ASSOCIATION / COLLECTIF ---
+"mon association","notre association",
+"creer une asso","créer une asso",
+"declarer association","déclarer association",
+"fonctionnement association","statut association",
+
+# --- MOTS FOURRE-TOUT MAIS UTILES ---
+"situation","probleme","problème",
+"information","renseignement","renseignements",
+"explication","aide","solution"
+
+
+
+]
